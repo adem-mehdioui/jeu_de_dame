de8b003bbc56de4672b32b788b46435ddb863e11
fares <skrfares@gmail.com>
2024-01-21 16:32:40 +0100

    New version of board.py

--- king.py
+++ king.py
@@ -1,3 +1,9 @@
+from checker_piece import checkerPiece
+
+class King(checkerPiece):
+    def __init__(self, color, row, col):
+        super().__init__(color, row, col)
+        self.is_king = True
 
 
         
